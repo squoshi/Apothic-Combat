6ec901f5b93438c38212c287809def7c901b8d73
muon-rw <pashah012@gmail.com>
2024-03-14 17:02:50 -0500

    Clean imports

--- src/main/java/dev/muon/apothiccombat/ApothicCombat.java
+++ src/main/java/dev/muon/apothiccombat/ApothicCombat.java
@@ -1,11 +1,8 @@
 package dev.muon.apothiccombat;
 
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.packs.repository.FolderRepositorySource;
-import net.minecraft.server.packs.repository.PackSource;
 import net.minecraftforge.common.ForgeMod;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.AddPackFindersEvent;
 import net.minecraftforge.event.ItemAttributeModifierEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -14,14 +11,6 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import net.minecraft.server.packs.PackType;
-import net.minecraftforge.event.AddPackFindersEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fml.loading.FMLPaths;
-import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 
 @Mod(ApothicCombat.MODID)
